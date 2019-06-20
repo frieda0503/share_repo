58eed6da184968d12840e5e45fb04241ad205af6
frieda <frieda@192.168.0.4>
2019-06-21 01:51:06 +0800

    修改folder

--- share/src/test/java/frieda/share/FizzBuzzStepTest.java
+++ share/src/test/java/frieda/share/FizzBuzzStepTest.java
@@ -2,12 +2,14 @@ package frieda.share;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 /**
  * Bdd Test
  */
 @RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:resources/FizzBuzz.feature")
 public class FizzBuzzStepTest {
 
 }
